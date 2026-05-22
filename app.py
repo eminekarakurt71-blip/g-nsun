9e5a345f95b8e0524f7ae2dfa54bb56288d4f047
eminekarakurt71-blip <eminekarakurt71@hotmail.com>
2026-05-23 00:26:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -527,4 +527,4 @@ def serve(path):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG", "True") == "True")
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
